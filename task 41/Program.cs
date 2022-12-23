907283ffa94e9b125ecceb6f711761c7e4fe7a0d
3JuIkA <rediskapopov27@gmail.com>
2022-12-23 13:36:00 +0200

    Added task 41

---
+++ task 41/Program.cs
@@ -0,0 +1,19 @@
+﻿int countOfPositive = 0;
+
+while (true)
+{
+    string answer = Console.ReadLine();
+    if (answer  == "stop") 
+    {
+        break;
+    }
+    else
+    {
+        double.TryParse(answer, out double answerNumber);
+        if (answerNumber > 0)
+        {
+            countOfPositive++;
+        }
+    }
+}
+Console.WriteLine(countOfPositive);
